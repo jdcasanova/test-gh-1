adf58a64818e5e5579b43f58d5efb3dd9e2481ff
jdavesa <86001040+jdavesa@users.noreply.github.com>
2022-02-17 11:37:06 +0100

    add text

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
     <div className="App">
       <h1>Hello World</h1>
       <p>My name is Peter but everybody call me Pepe</p>
+      <p>Este texto tiene que estar</p>
     </div>
   );
 }
