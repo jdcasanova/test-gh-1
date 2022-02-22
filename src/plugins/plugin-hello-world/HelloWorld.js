f961a2340ae4ac666f4f1698aa8e909a33bc2ba4
jdavesa <86001040+jdavesa@users.noreply.github.com>
2022-02-22 14:17:23 +0100

    commit 2

--- src/plugins/plugin-hello-world/HelloWorld.js
+++ src/plugins/plugin-hello-world/HelloWorld.js
@@ -1,5 +1,10 @@
 const HelloWorld = () => {
-    console.log('Hellow World');
-}
+  console.log("Hellow World");
+  return (
+    <div>
+      <h1>hola que ase</h1>
+    </div>
+  );
+};
 
 export default HelloWorld;
